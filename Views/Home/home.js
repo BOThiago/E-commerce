239924447bb5b729152d11f104177760aa8fb247
unknown <thiago.barreto.oliveira22@gmail.com>
2023-03-28 15:58:39 -0300

    Hide IP

--- Views/Home/home.js
+++ Views/Home/home.js
@@ -1,41 +1,41 @@
-window.onload = function() {
-  let token = getCookie("x-access-token");
-  // Se o token existir, envie uma solicitação POST para validar o token
-  if (token) {
-    let data = { "x-access-token": token };
+window.onload = function () {
+    let token = getCookie("x-access-token");
+    // Se o token existir, envie uma solicitação POST para validar o token
+    if (token) {
+        let data = { "x-access-token": token };
 
-    fetch("http://10.112.92.41:4335/home", {
-      method: "POST",
-      headers: {
-        "x-access-token": token
-      },
-    })
-    .then(response => {
-      if (response.status === 401) {
-        // Redireciona para a página de login se o token for inválido
+        fetch(`http://${IP}:4335/home`, {
+            method: "POST",
+            headers: {
+                "x-access-token": token,
+            },
+        })
+            .then((response) => {
+                if (response.status === 401) {
+                    // Redireciona para a página de login se o token for inválido
+                    window.location.href = "../Login/login.html";
+                } else {
+                    // faça algo com a resposta da API
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                // gerencie o erro
+            });
+    } else {
+        console.log("n existe");
+        // Redireciona para a página de login se o token não existir
         window.location.href = "../Login/login.html";
-      } else {
-        // faça algo com a resposta da API
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      // gerencie o erro
-    });
-  } else {
-    console.log('n existe');
-    // Redireciona para a página de login se o token não existir
-    window.location.href = "../Login/login.html";
-  }
+    }
 };
 // Função auxiliar para ler um cookie por nome
 function getCookie(name) {
-  let cookies = document.cookie.split("; ");
-  for (let i = 0; i < cookies.length; i++) {
-    let cookie = cookies[i].split("=");
-    if (cookie[0] === name) {
-      return cookie[1];
+    let cookies = document.cookie.split("; ");
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].split("=");
+        if (cookie[0] === name) {
+            return cookie[1];
+        }
     }
-  }
-  return null;
+    return null;
 }
--- Views/Login/login.js
+++ Views/Login/login.js
@@ -3,68 +3,69 @@ var btnSignup = document.querySelector("#signup");
 
 var body = document.querySelector("body");
 
-
 btnSignin.addEventListener("click", function () {
-   body.className = "sign-in-js"; 
+    body.className = "sign-in-js";
 });
 
 btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
-})
-
-document.getElementById("registro").addEventListener("submit", function(event) {
-    event.preventDefault(); // evita que o formulário seja enviado usando o método padrão
-    
-    var nameInput = document.getElementById("username");
-    var emailInput = document.getElementById("email");
-    var cpfInput = document.getElementById("cpf");
-    var passwordInput = document.getElementById("password");
-    dados = {
-        "nome": nameInput.value,
-        "email": emailInput.value,
-        "cpf": cpfInput.value,
-        "password": passwordInput.value
-    }
-    console.log(dados);
-    fetch('http://10.112.92.41:4335/signup', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(dados)      
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data); // faça algo com a resposta da API
-    })
-    .catch(error => {
-      console.error(error); // trate erros de rede ou da API
-    });
 });
-document.getElementById("login").addEventListener("submit", function(event) {
-  event.preventDefault(); // evita que o formulário seja enviado usando o método padrão
-  var emailcpfInput = document.getElementById("emailcpf");
-  var passwordloginInput = document.getElementById("passwordlogin");
-  dados = {
-      "login": emailcpfInput.value,
-      "password": passwordloginInput.value
-  }
-  fetch('http://10.112.92.41:4335/signin', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(dados)      
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    let token = data.token;
-    document.cookie = "x-access-token=" + token + "; path=/";
-    window.location.href = "../Home/home.html";
-     // faça algo com a resposta da API
-  })
-  .catch(error => {
-    console.error(error); // trate erros de rede ou da API
-  });
+
+document
+    .getElementById("registro")
+    .addEventListener("submit", function (event) {
+        event.preventDefault(); // evita que o formulário seja enviado usando o método padrão
+
+        var nameInput = document.getElementById("username");
+        var emailInput = document.getElementById("email");
+        var cpfInput = document.getElementById("cpf");
+        var passwordInput = document.getElementById("password");
+        dados = {
+            nome: nameInput.value,
+            email: emailInput.value,
+            cpf: cpfInput.value,
+            password: passwordInput.value,
+        };
+        console.log(dados);
+        fetch(`http://${IP}:4335/signup`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(dados),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data); // faça algo com a resposta da API
+            })
+            .catch((error) => {
+                console.error(error); // trate erros de rede ou da API
+            });
+    });
+document.getElementById("login").addEventListener("submit", function (event) {
+    event.preventDefault(); // evita que o formulário seja enviado usando o método padrão
+    var emailcpfInput = document.getElementById("emailcpf");
+    var passwordloginInput = document.getElementById("passwordlogin");
+    dados = {
+        login: emailcpfInput.value,
+        password: passwordloginInput.value,
+    };
+    fetch(`http://${IP}:4335/signin`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dados),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            let token = data.token;
+            document.cookie = "x-access-token=" + token + "; path=/";
+            window.location.href = "../Home/home.html";
+            // faça algo com a resposta da API
+        })
+        .catch((error) => {
+            console.error(error); // trate erros de rede ou da API
+        });
 });
